06362ecbe023067018014da5d0d2e2f92183497a
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-06-27 23:31:58 -0300

    update

---
+++ aulas/laco_loop_for.js
@@ -0,0 +1,17 @@
+// maneira ruim de fazer isso
+// console.log("estou aprendendor")
+// console.log("estou aprendendor")
+// console.log("estou aprendendor")
+// console.log("estou aprendendor")
+// console.log("estou aprendendor")
+
+
+// 1.FOR
+// 2.WHILE
+// 3.DO... WHILE
+// 4.FOR... IN
+// 5.FOR... OF
+
+for (let i = 0; i < 5; i++) {
+    console.log("estou aprendendo", i)
+}
