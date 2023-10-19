c528efb08a62b78db21ffd0a40438dfe5f892c5c
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-10-19 10:15:10 -0300

    update

--- freeCodeCamp/DOOM/aula-1/app.js
+++ freeCodeCamp/DOOM/aula-1/app.js
@@ -1,17 +1,32 @@
 // DOM manipulation
 
-// getElementById() Seleciona pelo ID
-const title = document.getElementById('main-heading')
-console.log(title)
+// // getElementById() Seleciona pelo ID
+// const title = document.getElementById('main-heading')
+// console.log(title)
 
-// getElementyByClassName() Seleciona pela CLASSE
-const  listItem = document.getElementsByClassName('list-items')
-console.log(listItem)
+// // getElementyByClassName() Seleciona pela CLASSE
+// const  listItem = document.getElementsByClassName('list-items')
+// console.log(listItem)
 
-// getElementByTagName() Seleciona pelo nome da tag
-const listItems = document.getElementsByTagName('li')
-console.log(listItems)
+// // getElementByTagName() Seleciona pelo nome da tag
+// const listItems = document.getElementsByTagName('li')
+// console.log(listItems)
 
-//querySelector() Seleciona tanto pelo ID, CLASSE e TAG
-const container = document.querySelector('div')
-console.log(container)
+// //querySelector() Seleciona o primeiro item encontrado tanto pelo ID, CLASSE e TAG
+// const container = document.querySelector('div')
+// console.log(container)
+
+// //querySelectorAll() Seleciona todos pelo ID, CLASSE e TAG
+// const containers = document.querySelectorAll('div')
+// console.log(container)
+
+// Acessar prioridades do CSS em JS
+const title = document.querySelector('#main-heading')
+title.style.color = 'red'
+
+const listItems = document.querySelectorAll('.list-items')
+for (i = 0; i < listItems.length; i++) {
+    listItems[i].style.fontSize = '25px'
+}
+
+// Criar elementos
