28708041b35b7d455902db308604c693a48c78a3
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-11-18 10:06:38 -0300

    update

---
+++ freeCodeCamp/JSFULL/P_4/index.js
@@ -0,0 +1,25 @@
+// let person = {
+//     name: 'Clécio',
+//     age: 21,
+//     country: 'Brazil'
+// }
+// function logData() {
+//     let text = `${person.name}, tem ${person.age} anos e mora no ${person.country}.`;
+//     console.log(text);
+// }
+// logData()
+
+
+// let age = 67;
+// if (age < 6) console.log('Free');
+// else if (age < 18) console.log('Desconto infantil');
+// else if (age < 27 ) console.log('Desconto de estudante');
+// else if (age < 67) console.log('Preço normal');
+// else console.log('Desconto de velhice');
+
+
+// console.log('Maiores paises:');
+// let largeCountries = ['China', 'India', 'USA', 'Indonesia', 'Pakistan']
+// for (let i = 0; i < largeCountries.length; i++) {
+//     console.log('- ' + largeCountries[i]);
+// }
