42d464c98b45403896d6eefedd98b4b7c06fa1fa
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-11-24 10:29:40 -0300

    update

--- freeCodeCamp/JSFULL/P_5/app.js
+++ freeCodeCamp/JSFULL/P_5/app.js
@@ -1,45 +1,55 @@
-let myLeads = [];
-// myLeads = JSON.parse(myLeads);
-// myLeads.push("www");
-// myLeads = JSON.stringify(myLeads);
-// console.log(typeof myLeads);
+let myLeads = []
+// myLeads = JSON.parse(myLeads)
+// myLeads.push("www")
+// myLeads = JSON.stringify(myLeads)
+// console.log(typeof myLeads)
 
-const inputEl = document.getElementById('input-el');
-const inputBtn = document.getElementById('input-btn');
-const ulEl = document.getElementById('ul-el');
-const deleteBtn = document.getElementById('delete-btn');
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
+const deleteBtn = document.getElementById('delete-btn')
+const tabBtn = document.getElementById('tab-btn')
 
-const leads = JSON.parse(localStorage.getItem('myLeads'));
+const leads = JSON.parse(localStorage.getItem('myLeads'))
 
 if (leads) {
-    myLeads = leads;
-    renderLeads();
+    myLeads = leads
+    render(myLeads)
 }
 
-function renderLeads() {
-    let listItems = "";
-    for(let i = 0; i < myLeads.length; i++) {
+function render(leads) {
+    let listItems = ""
+    for(let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+                <a href="${leads[i]}" target="_blank">${leads[i]}</a>
             </li>`
-        // const li = document.createElement('li');
-        // li.textContent = myLeads[i];
-        // ulEl.append(li);
+        // const li = document.createElement('li')
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
     }
-    ulEl.innerHTML = listItems;
+    ulEl.innerHTML = listItems
 }
 
 inputBtn.addEventListener('click', function() {
-    myLeads.push(inputEl.value);
-    localStorage.setItem('myLeads', JSON.stringify(myLeads));
-    renderLeads();
-    inputEl.value = "";
+    myLeads.push(inputEl.value)
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    render(myLeads)
+    inputEl.value = ""
 })
 
 deleteBtn.addEventListener('click', function() {
-    localStorage.clear();
-    myLeads = [];
-    renderLeads();
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
 })
 
+tabBtn.addEventListener('click', function() {
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){})
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem('myLeads', JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
---
+++ freeCodeCamp/JSFULL/P_5/parametros.js
@@ -0,0 +1,18 @@
+// function greeting(greeting, name) {
+//     console.log(greeting + ' ' + name);
+// }
+// greeting('Olá', 'Clécio')
+
+// //*          parametros
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// //* argumentos
+// add(3,4)
+// add(9, 102)
+
+function getFirst(arr) {
+    return arr[0];
+}
+arr = ['item1', 'item2', 'item3'];
+console.log(getFirst(arr));
